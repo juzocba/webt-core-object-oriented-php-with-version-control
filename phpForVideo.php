ecc37164cf481f6553acf9aac829de113213396d
aswa <alexander.nems@gmail.com>
2023-10-10 09:39:19 +0200

    Updates US 7

--- phpForVideo.php
+++ phpForVideo.php
@@ -6,16 +6,14 @@ interface VideoInterface {
         public function getHTML();
 }
 
-class Video implements VideoInterface {
-    private $name;
-    private $source;
-    private $html;
+abstract class AbstractVideo {
+   private $name;
+   private $source;
 
-    function __construct($name, $source, $html) {
+   function __construct($name, $source) {
         $this->name = $name;
         $this->source = $source;
-        $this->html = $html;
-      }
+   }
 
     function getName() {
        return $this->name;
@@ -24,14 +22,20 @@ class Video implements VideoInterface {
     function getSource() {
        return $this->source;
     }
-
-    function getHTML() {
-       return $this->html;
-    }
 }
 
-$video = new Video('Video Title', 'https://example.com/video.mp4', '<iframe src="https://example.com/embed"></iframe>');
 
-    echo 'Video Name: ' . $video->getName();
-    echo 'Video Source: ' . $video->getSource();
-    echo 'Embed HTML: ' . $video->getHTML();
+
+
+class Youtube extends AbstractVideo implements VideoInterface {
+   private $html;
+
+   function __construct($name, $source, $html) {
+      parent::__construct($name, $source);
+      $this->html = $html;
+    }
+
+   function getHTML() {
+      return '<iframe src = "' . $this->html . '"></iframe>';
+   }
+}
---
+++ youtubevideos_updated.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+    <style>
+        .videoentry {
+            width:100%;
+            height: auto;
+        }
+    body {
+        margin: 0;
+        padding: 0;
+        font-family: Arial, sans-serif;
+    }
+
+    </style>
+
+<body>
+    <header>
+        <h1>Plain HTML</h1>
+        <p>This is my updated plain html site</p>
+    </header>
+    <div class="videoentry">
+       <?php
+            include 'phpForVideo.php';
+            $video = new Youtube('YouTube Video', 'YouTube', 'https://www.youtube.com/embed/RQkdB49hBTc?si=jwfaWJfC1gRrSNC8');
+            for($i = 0; $i < 10; $i++) {
+                echo $video->getHTML();
+            }
+       ?>
+        
+    </div>
+</body>
+
+</html>
