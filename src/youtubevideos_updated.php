196066fc58de58aa37fad55c74e1d3cbeb360316
aswa <alexander.nems@gmail.com>
2023-10-21 23:08:29 +0200

    basic css
    basic css, vimeo und youtube videos müssen aber noch kombiniert werden

--- src/youtubevideos_updated.php
+++ src/youtubevideos_updated.php
@@ -1,36 +1,54 @@
 <!DOCTYPE html>
 <html lang="en">
-    <style>
-        .videoentry {
-            width:100%;
-            height: auto;
+<head>
+<style>
+        body {
+            height: 100%;
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
         }
-    body {
-        margin: 0;
-        padding: 0;
-        font-family: Arial, sans-serif;
-    }
 
+        header {
+            text-align: center;
+            margin-bottom: 10px;
+        }
+
+        .videoentry {
+            background-color: #C7E4FF;
+            border-radius: 5px;
+            padding-top: 15px;
+            padding-bottom: 15px;
+            padding-right: 100px;
+            padding-left: 100px;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            position: relative;
+        }
     </style>
-
+</head>
 <body>
     <header>
-        <h1>Plain HTML</h1>
-        <p>This is my updated plain html site</p>
+        <h1>Video.net</h1>
+        <p>Have fun on our webpage, you can watch several YouTube and Vimeo videos</p>
     </header>
     <div class="videoentry">
-       <?php
-            include 'Youtube.php';
-            include 'Vimeo.php';
-            $video = new Youtube('Test', 'YouTube', 'https://www.youtube.com/embed/RQkdB49hBTc?si=jwfaWJfC1gRrSNC8');
-            for($i = 0; $i < 10; $i++) {
-                echo $video->getName() . '\n';
-                echo $video->getSource() . '\n';
-                echo $video->getHTML() . '\n';
-            }
-       ?>
-        
+        <?php
+        include 'Youtube.php';
+        $video = new Youtube('Test', 'YouTube', 'https://www.youtube.com/embed/RQkdB49hBTc?si=jwfaWJfC1gRrSNC8');
+        for ($i = 0; $i < 10; $i++) {
+            echo ($i+1) . ": " . $video->getName() . ", uploaded on ";
+            echo $video->getSource() . "<br>";
+            echo $video->getHTML() . "<br>";
+            echo "<hr>";
+        }
+        ?>
     </div>
 </body>
-
 </html>
